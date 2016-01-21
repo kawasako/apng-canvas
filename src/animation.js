fd707f9815c0f373f64f057d79b1d703fe26311f
kawasako <kohei.kawasaki@linecorp.com>
2016-01-21 11:16:20 +0900

    remove requestAnimationFrame pollution

--- src/animation.js
+++ src/animation.js
@@ -5,6 +5,34 @@
  * @constructor
  */
 var Animation = function () {
+
+    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+    // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+    // MIT license
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    var requestAnimationFrame, cancelAnimationFrame;
+    for(var x = 0; x < vendors.length && !requestAnimationFrame; ++x) {
+        requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+    if (!requestAnimationFrame) {
+        requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 50); // fix 20fps
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!cancelAnimationFrame) {
+        cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+    }
+
     // Public
 
     this.width = 0;
